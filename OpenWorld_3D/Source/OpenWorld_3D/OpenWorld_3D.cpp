ddfb81a9bc6c8a5edb64b7782d9bc9ccce727454
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-06-18 20:32:53 +0900

    init

---
+++ OpenWorld_3D/Source/OpenWorld_3D/OpenWorld_3D.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OpenWorld_3D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OpenWorld_3D, "OpenWorld_3D" );
