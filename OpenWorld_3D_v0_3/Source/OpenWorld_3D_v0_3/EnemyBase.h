7e81e13cb81c7c5a6cae9a4b9788c4954cb80319
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-07-03 01:16:07 +0900

    make Enemy Base Class & Weapon Class

--- OpenWorld_3D_v0_3/Source/OpenWorld_3D_v0_3/EnemyBase.h
+++ OpenWorld_3D_v0_3/Source/OpenWorld_3D_v0_3/EnemyBase.h
@@ -2,9 +2,13 @@
 
 #pragma once
 
+#include "WeaponBase.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "EnemyBase.generated.h"
+
+class AWeaponBase;
+DECLARE_DELEGATE(FDele_Single);
 
 UCLASS()
 class OPENWORLD_3D_V0_3_API AEnemyBase : public ACharacter
@@ -15,6 +19,13 @@ public:
 	// Sets default values for this character's properties
 	AEnemyBase();
 
+private:
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Equip", meta = (AllowPrivateAccess = "true"))
+	AWeaponBase* WeaponActor;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Equip", meta = (AllowPrivateAccess = "true"))
+	bool bIsWieldingWeapon;
+
+	FDele_Single Fuc_DeleSingle;
 protected:
 
 };
---
+++ OpenWorld_3D_v0_3/Source/OpenWorld_3D_v0_3/WeaponBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class OPENWORLD_3D_V0_3_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+};
